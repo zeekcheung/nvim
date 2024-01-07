5935051e68026989caf197c997c93a3ce6c2046e
zeekcheung <zeekcheung@gmail.com>
2024-01-07 12:21:16 +0800

    feat: add configurations for typescript

---
+++ lua/plugins/lang/typescript.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'typescript', 'tsx' })
+      end
+    end,
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      -- Make sure mason installs the server
+      servers = {
+        tsserver = {
+          keys = {
+            {
+              '<leader>co',
+              function()
+                vim.lsp.buf.code_action {
+                  apply = true,
+                  context = {
+                    only = { 'source.organizeImports.ts' },
+                    diagnostics = {},
+                  },
+                }
+              end,
+              desc = 'Organize Imports',
+            },
+            {
+              '<leader>cR',
+              function()
+                vim.lsp.buf.code_action {
+                  apply = true,
+                  context = {
+                    only = { 'source.removeUnused.ts' },
+                    diagnostics = {},
+                  },
+                }
+              end,
+              desc = 'Remove Unused Imports',
+            },
+          },
+          settings = {
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'eslint_d' })
+    end,
+  },
+}
