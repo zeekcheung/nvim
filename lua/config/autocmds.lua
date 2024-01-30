b26d5cd763436248e2d31413050af4b5b04121e0
zeekcheung <zeekcheung@gmail.com>
2024-01-30 09:54:23 +0800

    feat: jumpt to last edit position when opening files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -136,3 +136,15 @@ autocmd('BufWritePost', {
     vim.fn.winrestview(winview)
   end,
 })
+
+-- Jumpt to last edit position when opening files
+autocmd('BufReadPost', {
+  group = augroup('last_edit_position', { clear = true }),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
